0056191928dc9222a0988688ad568fa15d76869e
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-04-26 16:44:50 +0300

    Разбираюсь в библиотеке

--- first_tg_bot/main.py
+++ first_tg_bot/main.py
@@ -6,22 +6,20 @@ import requests
 from aiogram import Bot, Dispatcher # type: ignore
 from aiogram.filters import CommandStart, Command # type: ignore
 from aiogram.types import Message # type: ignore
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 if (requests.get("https://api.telegram.org").status_code == 200):
     print("Соединение с api.telegram.org прошло успешно")
 else:
     print("Ошибка. Нет соединения с api.telegram.org")
 
-# выдаёт 200, значит успешно
-
 dp = Dispatcher()
 load_dotenv()
 
 user_messages = {} 
 
-async def send_and_track(message: Message, text: str):
+async def send_and_track(message: Message, text: str, **kwargs):    # туть остановился :)
     user_id = message.from_user.id
-
     user_messages.setdefault(user_id, [])  
     
     user_messages[user_id].append(message.message_id)
@@ -33,8 +31,32 @@ async def send_and_track(message: Message, text: str):
     return msg
 
 @dp.message(CommandStart())
-async def start_handler(message: Message) -> None:
-    await send_and_track(message, "Привет! Я clanker. Могу только повторять тебя).")
+async def start_handler(message: Message):
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Добро пожаловать в стартовое меню", callback_data="click")],
+        [InlineKeyboardButton(text="Очистить", callback_data="clear")]
+    ])
+
+    await message.answer("Выберите опцию:", reply_markup=keyboard)
+
+@dp.callback_query()
+async def handle_callback(callback: CallbackQuery):
+    await callback.answer()
+
+    if callback.data == "click":
+        await callback.message.answer("Фишки работают!!!")
+
+    elif callback.data == "clear":
+        user_id = callback.from_user.id
+        ids = user_messages.get(user_id, [])
+
+        if ids:
+            await callback.bot.delete_messages(
+                chat_id = callback.message.chat.id,
+                message_ids = ids
+            )
+
+        user_messages[user_id] = []
 
 @dp.message(Command("clear"))
 async def clear_handler(message: Message):
